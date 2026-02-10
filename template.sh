c44bafab282b631f202cd3a037ecc1c609d490f2
KAVISHRI N <kavishrinatarajan@gmail.com>
2026-02-10 10:50:41 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,20 @@
+# creating directory
+
+mkdir -p src
+mkdir -p research
+
+# craeting files
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch reqiurements.txt
+
+
+
+echo "files created succesfully"
+
